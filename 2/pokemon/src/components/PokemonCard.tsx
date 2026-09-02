81f09b254e1049b553206b41bf057371b9eade14
basttouuu <hug0.paulier83@gmail.com>
2026-09-02 17:23:44 +0200

    Troisieme Quete

--- 2/pokemon/src/components/PokemonCard.tsx
+++ 2/pokemon/src/components/PokemonCard.tsx
@@ -1,11 +1,33 @@
-function PokemonCard () {
-  return (
-    <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-      <figcaption>Bulbasaur</figcaption>
-    </figure>
 
-  );
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+
+/** site utiliser pour m'aider a afficher les cartes des pokémons : 
+ * https://fr.legacy.reactjs.org/docs/lists-and-keys.html */
+
+
+function PokemonCard () {
+    return (
+        <div>
+            {pokemonList.map((pokemon) => (
+         
+            <figure>
+                {pokemon.imgSrc !== undefined ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}            
+                <figcaption>{pokemon.name}</figcaption>
+            </figure>
+
+            ))}
+        </div>
+    );
 }
 
 export default PokemonCard;
